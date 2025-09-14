621f6c03046d1d6c424d04d0876013e807732a6e
Muhammad Fizan Tariq <muhammadfizantariq@example.com>
2025-09-14 03:15:14 -0700

    Fix remaining asset paths for production

--- src/Pages/WatchReel.jsx
+++ src/Pages/WatchReel.jsx
@@ -1,7 +1,7 @@
-import React from "react";
-import Layout from "../Components/Layout";
-import reelVideo from "/src/assets/reelbg.mp4";
-import reelBgTransparent from "/src/assets/reel.png"; 
+import React, { useRef, useState, useEffect } from "react";
+import { motion } from "framer-motion";
+import reelVideo from "../assets/reelbg.mp4";
+import reelBgTransparent from "../assets/reelbg_transparent.png"; 
 
 
 const WatchReel = () => {
